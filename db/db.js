a45e049732080e99cdbd9cd0cbbf1ed747a976a2
Aman094846 <amandawar180@gmail.com>
2025-07-18 18:53:59 +0530

    mongoAtlas

--- db/db.js
+++ db/db.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log("MongoDB Connected");
-    }).catch((err) => {
-        console.error("MongoDB connection error:", err);
-    });
-
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI).then(
+        () => {console.log("MongoDB_Connected")}
+    ).catch(err => {console.log(err)});
 }
 
 
