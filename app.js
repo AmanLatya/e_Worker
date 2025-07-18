1a9740f3afdda4a28f830b2d533b7b5ebde16cb8
Aman094846 <amandawar180@gmail.com>
2025-07-18 18:43:27 +0530

    add mongo atlas

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const workerRoutes = require("./routes/worker");
 const meRoutes = require("./routes/me");
 const requestRoutes = require("./routes/request-routes");
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:7000', 'https://your-frontend-domain.com'], // adjust accordingly
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
